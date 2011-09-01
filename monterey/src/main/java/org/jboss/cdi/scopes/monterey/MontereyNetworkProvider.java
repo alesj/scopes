5cd6b779e338a39377203001f792a11637a03e88
Ales Justin <ales.justin@gmail.com>
2011-09-01 22:48:04 +0200

    Initial commit

---
+++ monterey/src/main/java/org/jboss/cdi/scopes/monterey/MontereyNetworkProvider.java
@@ -0,0 +1,75 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.cdi.scopes.monterey;
+
+import com.cloudsoftcorp.monterey.servicebean.access.api.MontereyNetworkEndpoint;
+import com.cloudsoftcorp.monterey.servicebean.access.api.MontereyNetworkEndpointImpl;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Monterey network provider.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@ApplicationScoped
+public class MontereyNetworkProvider {
+
+    @Produces @ApplicationScoped
+    public MontereyNetworkEndpoint createEndpoint() throws Exception {
+        Properties p = loadProperties();
+		MontereyNetworkEndpointImpl network = new MontereyNetworkEndpointImpl();
+		network.setManagementNodeUrl(p.getProperty("managementNodeUrl"));
+		network.setUsername(p.getProperty("username"));
+		network.setPassword(p.getProperty("password"));
+		network.setLocation(p.getProperty("location"));
+		network.setHasPrivateIp(Boolean.parseBoolean(p.getProperty("hasPrivateIp")));
+		network.start();
+        return network;
+    }
+
+    public void shutdownEndpoint(@Disposes MontereyNetworkEndpoint endpoint) {
+        endpoint.shutdown();
+    }
+
+    protected Properties loadProperties() throws IOException {
+        ClassLoader cl = getClass().getClassLoader();
+        InputStream is = cl.getResourceAsStream("monterey.properties");
+        try {
+            Properties properties = new Properties();
+            properties.load(is);
+            return properties;
+        } finally {
+            try {
+                is.close();
+            } catch (IOException ignored) {
+            }
+        }
+    }
+
+}
---
+++ monterey/src/main/java/org/jboss/cdi/scopes/monterey/MontereyScopedContext.java
@@ -0,0 +1,62 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.cdi.scopes.monterey;
+
+import javax.enterprise.context.spi.Context;
+import javax.enterprise.context.spi.Contextual;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.lang.annotation.Annotation;
+
+/**
+ * Monterey scoped context.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@Singleton
+public class MontereyScopedContext implements Context {
+    private final BeanManager manager;
+
+    @Inject
+    public MontereyScopedContext(BeanManager manager) {
+        this.manager = manager;
+    }
+
+    public Class<? extends Annotation> getScope() {
+        return MontereyScoped.class;
+    }
+
+    public <T> T get(Contextual<T> contextual, CreationalContext<T> creationalContext) {
+        return null;  // TODO
+    }
+
+    public <T> T get(Contextual<T> contextual) {
+        return get(contextual, null);
+    }
+
+    public boolean isActive() {
+        return true;
+    }
+}
