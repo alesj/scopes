2ccf19c8a799459b2fe1acff4b1ac4c3aee922f9
Ales Justin <ales.justin@gmail.com>
2011-09-05 13:52:28 +0200

    Configure testing.

---
+++ monterey/src/test/java/org/jboss/test/seam/scopes/monterey/SimpleTestCase.java
@@ -0,0 +1,67 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test.seam.scopes.monterey;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.seam.scopes.monterey.MontereyExtension;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.enterprise.inject.spi.Extension;
+
+/**
+ * Simple test.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@RunWith(Arquillian.class)
+public class SimpleTestCase {
+
+    @Deployment
+    public static Archive getSmokeDeployment() {
+        JavaArchive archive = ShrinkWrap.create(JavaArchive.class)
+                .addAsResource("monterey.properties")
+                .addAsManifestResource("smoke/META-INF/beans.xml", "beans.xml")
+                .addPackage(SimpleTestCase.class.getPackage());
+        addMontereyExtension(archive);
+        return archive;
+    }
+
+    protected static void addMontereyExtension(JavaArchive archive) {
+        archive.addAsResource(new StringAsset(MontereyExtension.class.getName()), "META-INF/services/" + Extension.class.getName());
+        archive.addPackage(MontereyExtension.class.getPackage());
+    }
+
+    @Test
+    public void testSmoke(MService mService) throws Exception {
+        Assert.assertNotNull(mService);
+        mService.play("Spade Ace", "Heart Jack");
+    }
+
+}
